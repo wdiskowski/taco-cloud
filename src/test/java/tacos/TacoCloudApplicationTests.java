70a6722c8de99b3f15851243b1f735d616148422
wladi <wladi@wladi-Latitude-E6430>
2019-12-14 21:16:30 +0100

    init branch

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,11 +1,13 @@
 package tacos;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TacoCloudApplicationTests {
 
+	@DisplayName("context load")
 	@Test
 	void contextLoads() {
 	}
