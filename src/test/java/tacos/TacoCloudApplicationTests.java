be3e0b95020e29b9dad03d999b4978f1b8a288ac
wdiskowski <wdiskowski@gmail.com>
2019-12-14 22:04:29 +0100

    test smartgit

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TacoCloudApplicationTests {
 
-	@DisplayName("context load")
+	@DisplayName("context load.")
 	@Test
 	void contextLoads() {
 	}
